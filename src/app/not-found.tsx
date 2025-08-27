c04495a5da35ea98a1eeb680c4de3f793177950c
tktkaws <ttkaws15@gmail.com>
2025-08-27 16:27:52 +0900

    add: 404ページ

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-xl font-bold">404</h1>
+      <p>ページが見つかりませんでした</p>
+    </main>
+  );
+}
