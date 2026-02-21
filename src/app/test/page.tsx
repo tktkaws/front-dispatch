3a803cece9e681cc502d496e7e66d5e5e434a97e
Takayuki Takahashi <ttkaws15@gmail.com>
2026-02-21 09:53:52 +0900

    add: Wireframe47コンポーネントを追加し、画像とテキストを表示

---
+++ src/app/test/page.tsx
@@ -0,0 +1,95 @@
+const imgLine6 = "http://localhost:3845/assets/a4dcdc46556dd9a4d9391e1bd03748d25dc07d99.svg";
+const imgLine8 = "http://localhost:3845/assets/bafc87383cf6db1e29739aafd513435c6991a177.svg";
+const imgLine7 = "http://localhost:3845/assets/327aa1baf50e7b8c138fe3449ea56db0e1cd948d.svg";
+
+export default function Wireframe47() {
+  return (
+    <div className="bg-[#ffffff] relative size-full" data-name="Wireframe - 47" data-node-id="743:1000">
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[42px] not-italic text-[#000000] text-[24px] text-nowrap top-[52.5px] translate-y-[-50%]" data-node-id="743:995">
+        <p className="leading-[normal] whitespace-pre">[Front Dispatch]</p>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[1320px] not-italic text-[#000000] text-[16px] text-nowrap top-[51.5px] translate-y-[-50%]" data-node-id="743:1032">
+        <p className="leading-[normal] whitespace-pre">[Config]</p>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[43px] not-italic text-[#000000] text-[16px] text-nowrap top-[97.5px] translate-y-[-50%]" data-node-id="743:997">
+        <p className="leading-[normal] whitespace-pre">Blog</p>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[253.5px] translate-y-[-50%]" data-node-id="743:1008">
+        <p className="leading-[normal] whitespace-pre">Next.js</p>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[42px] not-italic text-[#000000] text-[16px] text-nowrap top-[143.5px] translate-y-[-50%]" data-node-id="743:998">
+        <p className="leading-[normal] whitespace-pre">About</p>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[43px] not-italic text-[#000000] text-[16px] text-nowrap top-[205.5px] translate-y-[-50%]" data-node-id="743:1001">
+        <p className="leading-[normal] whitespace-pre">/Filter</p>
+      </div>
+      <div className="absolute flex h-[1px] items-center justify-center left-[43px] top-[223px] w-[229.998px]">
+        <div className="flex-none rotate-[359.751deg]">
+          <div className="h-0 relative w-[230.002px]" data-node-id="743:1003">
+            <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+              <img alt className="block max-w-none size-full" src={imgLine6} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-0 left-[42px] top-[114px] w-[42px]" data-node-id="743:1027">
+        <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+          <img alt className="block max-w-none size-full" src={imgLine8} />
+        </div>
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[322px] not-italic text-[#000000] text-[16px] text-nowrap top-[205.5px] translate-y-[-50%]" data-node-id="743:1004">
+        <p className="leading-[normal] whitespace-pre">/Articles</p>
+      </div>
+      <div className="absolute h-0 left-[322px] top-[223.02px] w-[1075px]" data-node-id="743:1005">
+        <div className="absolute bottom-0 left-0 right-0 top-[-1px]">
+          <img alt className="block max-w-none size-full" src={imgLine7} />
+        </div>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[248px]" data-node-id="743:1007">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[281.5px] translate-y-[-50%]" data-node-id="743:1010">
+        <p className="leading-[normal] whitespace-pre">HTML</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[276px]" data-node-id="743:1011">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[309.5px] translate-y-[-50%]" data-node-id="743:1013">
+        <p className="leading-[normal] whitespace-pre">CSS</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[304px]" data-node-id="743:1014">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[337.5px] translate-y-[-50%]" data-node-id="743:1015">
+        <p className="leading-[normal] whitespace-pre">Accessibility</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[332px]" data-node-id="743:1016">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[365.5px] translate-y-[-50%]" data-node-id="743:1018">
+        <p className="leading-[normal] whitespace-pre">AI</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[360px]" data-node-id="743:1019">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[393.5px] translate-y-[-50%]" data-node-id="743:1020">
+        <p className="leading-[normal] whitespace-pre">Other</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[388px]" data-node-id="743:1021">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[421.5px] translate-y-[-50%]" data-node-id="743:1022">
+        <p className="leading-[normal] whitespace-pre">Reading</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[416px]" data-node-id="743:1023">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+      <div className="absolute flex flex-col font-mono justify-center leading-[0] left-[69px] not-italic text-[#000000] text-[16px] text-nowrap top-[449.5px] translate-y-[-50%]" data-node-id="743:1024">
+        <p className="leading-[normal] whitespace-pre">Design</p>
+      </div>
+      <div className="absolute bg-[rgba(217,217,217,0)] left-[42px] size-4 top-[444px]" data-node-id="743:1025">
+        <div aria-hidden="true" className="absolute border border-[#000000] border-solid inset-0 pointer-events-none" />
+      </div>
+    </div>
+  );
+}
